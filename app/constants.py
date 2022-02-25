d41cbe025c571ec03c2bf1688ac1970f954704d7
Andrew Chen <andrewc@bond.tech>
2022-02-24 21:35:57 -0500

    evaluate sandbox env correctly

--- app/constants.py
+++ app/constants.py
@@ -1,15 +1,13 @@
-
 from os import environ
 from os.path import exists
 from dotenv import load_dotenv
 
 # load .env file if it exists (not under systemd or kubernetes)
-if exists('.env'): 
+if exists(".env"):
     load_dotenv()
 
-identity      = environ['IDENTITY'] 
-authorization = environ['AUTHORIZATION']
-live          = "LIVE" in environ
+identity = environ["IDENTITY"]
+authorization = environ["AUTHORIZATION"]
+live = environ.get("LIVE", "false").lower().startswith("t")
 
 bond_host = "https://api.bond.tech" if live else "https://sandbox.bond.tech"
-
